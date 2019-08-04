67e66c0ca4733ee2348b994c098912bf75d2f370
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-04 09:13:18 +0200

    add jumbotron

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{$title}}</h1>
-<p>This is a Laravel BlogApp by Jade Rickerts</p>
+    <div class="jumbotron text-centre">
+        <h1>Welcome to my Blog App</h1>
+        <p class="lead">This is a Laravel blog application by Jade Rickerts</p>
+    </div>
 @endsection
