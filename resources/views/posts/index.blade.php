283d2a7e20afa6bc998b6cacc73e89bfc423c7e5
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-04 12:44:55 +0200

    pass posts to post index view. show post titles in view.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Posts</h1>
+    <h3>Posts</h3>
+    @if(count($posts) > 0)
+        @foreach ($posts as $post)
+            <div class="well">
+                <h3>{{$post->$title}}</h3>
+            </div>
+        @endforeach
+    @else
+        <p>No posts found</p>
+    @endif
 @endsection
