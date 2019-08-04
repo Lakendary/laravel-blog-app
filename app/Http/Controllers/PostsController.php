54445d65f00c4cf92862c24edd61db2907793c80
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-04 17:43:06 +0200

    add comments tp post controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+//if you want to use standard MySql queries
+//use DB;
 
 class PostsController extends Controller
 {
@@ -15,6 +17,11 @@ class PostsController extends Controller
     public function index()
     {
         //$posts = Post::all();
+        //return Post::where('title', 'Post Two')->get();
+        //$posts = DB::select('SELECT * FROM posts');
+        //$posts = Post::orderBy('title', 'desc')->take(1)->get();
+        //$posts = Post::orderBy('title', 'desc')->get();
+
         $posts = Post::orderBy('title', 'desc')->get();
         return view('posts.index')->with('posts', $posts);
     }
