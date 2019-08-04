143a0904458a76e5b86969ff84509fd3075e1086
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-04 20:32:33 +0200

    add form to create post view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,5 +3,10 @@
 @section('content')
     <h1>Create Post</h1>
     <br>
-
+    {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+        </div>
+    {!! Form::close() !!}
 @endsection
