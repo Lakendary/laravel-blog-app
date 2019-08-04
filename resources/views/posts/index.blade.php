473d6348fa0d383056f1a6c50ef4af22623c8f9b
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-04 17:10:55 +0200

    change well into card

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,9 +4,13 @@
     <h1>Posts</h1>
     @if(count($posts) > 1)
         @foreach ($posts as $post)
-            <div class="well">
-                <h3>{{$post->title}}</h3>
-                <small>Written on {{$post->created_at}}</small>
+            <div class="card">
+                <div class="card-body">
+                    <h3>{{$post->title}}</h3>
+                    <p>Excerpt comes here!</p>
+                    <small>Written on {{$post->created_at}}</small>
+                    <a href="#" class="btn btn-primary">View Post</a>
+                </div>
             </div>
         @endforeach
     @else
