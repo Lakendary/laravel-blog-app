d41605f283f5732128939b6e43d4a2ef54a26892
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-04 12:26:14 +0200

    add rource route for posts

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@
 // });
 
 Route::get('/', 'PagesController@index');
-
 Route::get('/about', 'PagesController@about');
-
 Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostsController');
