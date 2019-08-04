7e1cb823cbd2281595f6e010d48bafda0f173b6e
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-04 17:31:03 +0200

    order all posts by title, descending

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        //$posts = Post::all();
+        $posts = Post::orderBy('title', 'desc')->get();
         return view('posts.index')->with('posts', $posts);
     }
 
