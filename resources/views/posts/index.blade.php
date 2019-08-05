31efc80ca452ee2345aea06c5d24eb0da2a81bff
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-05 09:38:46 +0200

    add user name to each post on index view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-body">
                     <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
                     <p>Excerpt comes here!</p>
-                    <small>Written on {{$post->created_at}}</small>
+                    <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
                     <br>
                     <a href="/posts/{{$post->id}}" class="btn btn-primary">View Post</a>
                 </div>
