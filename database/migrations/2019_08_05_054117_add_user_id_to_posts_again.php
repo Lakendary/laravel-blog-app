84d4804b89e4da9ec9c779c757f60503a6108931
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-05 07:42:17 +0200

    delete old migration and create new one

--- database/migrations/2019_08_05_054117_add_user_id_to_posts_again.php
+++ database/migrations/2019_08_05_054117_add_user_id_to_posts_again.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserIdToPosts extends Migration
+class AddUserIdToPostsAgain extends Migration
 {
     /**
      * Run the migrations.
