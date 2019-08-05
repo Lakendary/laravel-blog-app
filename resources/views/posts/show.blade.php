d81e4c285b75549f4d256f9391e674b3e45f89ac
Jade Rickerts <jade.rickerts@gmail.com>
2019-08-05 05:28:40 +0200

    add back and edit button to posts show page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    <a href="/posts" class="btn btn-default">Back</a>
     <h1>{{$post->title}}</h1>
-    <small>Written on {{$post->created_at}}</small>
     <div>
         {!!$post->body!!}
     </div>
+    <br>
+    <small>Written on {{$post->created_at}}</small>
+    <br>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 @endsection
